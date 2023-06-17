b16d881575cf192c8152ef464a9315001033224b
NikitaKuchukov <Kuchukovnik@mail.ru>
2023-06-17 16:59:13 +0700

    Initial commit

---
+++ src/test/java/ru/skypro/lessons/springboot/springweb/SpringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.lessons.springboot.springweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
