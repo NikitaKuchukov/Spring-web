2f65cda66d5c6c8cabf2a6dfcc13e7c7afd6ea5d
NikitaKuchukov <Kuchukovnik@mail.ru>
2023-07-20 20:03:03 +0700

    homework done

--- src/main/java/ru/skypro/lessons/springboot/springweb/controller/InfoController.java
+++ src/main/java/ru/skypro/lessons/springboot/springweb/controller/InfoController.java
@@ -1,21 +1,19 @@
 package ru.skypro.lessons.springboot.springweb.controller;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Profile("dev")
 @RequestMapping("info")
 public class InfoController {
 
-    @Value("${dev}")
-    private String dev;
+    @Value("${app.env}")
+    private String profile;
 
     @GetMapping()
     public String appInfo() {
-        return dev;
+        return profile;
     }
 }
